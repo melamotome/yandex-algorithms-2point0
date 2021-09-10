eb0a9c87344c58fb84fdf0e7efa730fc869a7968
melamotome <wisterinafis@mail.ru>
2021-09-10 12:55:14 +0300

    Initial commit

---
+++ src/bTrack/_1A.java
@@ -0,0 +1,15 @@
+package bTrack;
+
+import java.util.Scanner;
+
+public class _1A {
+
+    private void run() {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+    }
+
+    public static void main(String[] args) {
+        new _1A().run();
+    }
+}
